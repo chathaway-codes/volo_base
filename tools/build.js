6841471c6e2ee13f490b0ea52a7b8c9659faa901
Charles H <chathaway@logrit.com>
2013-01-17 18:31:46 -0500

    Populated with some files

---
+++ tools/build.js
@@ -0,0 +1,13 @@
+{
+    "appDir": "../js",
+    "baseUrl": "lib",
+    "paths": {
+        "app": "../app"
+    },
+    "dir": "../js-built",
+    "modules": [
+        {
+            "name": "app"
+        }
+    ]
+}
